eb76a3015cb21e649ddcb2e7a3437fac9b48cd13
Jimwel Trabado Anobong <anobongjimwel@outlook.ph>
2018-10-08 03:06:03 +0800

    Some finishing touches
    Finishing touches on the scheduling module and its all done

--- classes/Scheduler.php
+++ classes/Scheduler.php
@@ -10,7 +10,7 @@
             const THURSDAY = 4;
             const FRIDAY = 5;
             const SATURDAY = 6;
-            const SUNDAY = 0;
+            const SUNDAY = 7;
             private $pdo;
             private $username, $password, $destination;
             public function __construct()
@@ -21,12 +21,16 @@
                 $this->pdo = new PDO($this->destination, $this->username, $this->password);
             }
 
-            public function compareDate($dayNum, $date) {
-                if (date(strtotime($date),'N') == $dayNum) {
-                    return true;
-                } else {
-                    return false;
+            public function compareDate($date) {
+                $theSame = false;
+                $getDaysinSchedule = $this->pdo->query("SELECT prefValue FROM settings WHERE prefName = 'schedules'");
+                $days = explode(', ', $getDaysinSchedule->fetch(PDO::FETCH_ASSOC)['prefValue']);
+                foreach ($days as $day) {
+                    if (date('N',strtotime($date)) == $day) {
+                        $theSame = true;
+                    }
                 }
+                return $theSame;
             }
 
             public function getWeekLongDates($date) {
@@ -61,12 +65,59 @@
 
             public function getMonthLongDates($date) {
                 $dates = array();
-                for ($i=1;$i<=7;$i++) {
+                for ($i=1;$i<=31;$i++) {
                     $currentDate = date_create($date);
                     date_add($currentDate, date_interval_create_from_date_string($i.' days'));
                     array_push($dates, $currentDate->format('Y-m-d'));
                 }
                 return $dates;
+            }
+
+            public function changeSchedules ($days) {
+                $dayStr = '';
+                foreach ($days as $day) {
+                    $dayStr.=$day.', ';
+                }
+                $dayStr = strlen($dayStr)!=0 ? substr($dayStr, 0, -2) : $dayStr;
+                $setSchedules = $this->pdo->prepare("UPDATE settings SET prefValue = '$dayStr' WHERE prefName = 'schedules'");
+                $setSchedules->execute();
+            }
+
+            public function clearSchedules() {
+                $setSchedules = $this->pdo->prepare("UPDATE settings SET prefValue = '' WHERE prefName = 'schedules'");
+                $setSchedules->execute();
+            }
+
+            public function getSchedules() {
+                $queryForSchedules = $this->pdo->query("SELECT prefValue FROM settings WHERE prefName = 'schedules'");
+                $schedules = array();
+                foreach(explode(', ', $queryForSchedules->fetch(PDO::FETCH_ASSOC)) as $schedule) {
+                    array_push($schedules, $schedule);
+                }
+                return $schedules;
+            }
+
+            public function returnLongDateFmt($date) {
+                return date('l, F d, Y, (e, T P)', strtotime($date));
+            }
+
+            public function returnShortMonth($date) {
+                return mb_strtoupper(date('M', strtotime($date)));
+            }
+
+            public function returnDay($date) {
+                return date('d', strtotime($date));
+            }
+
+            public function getIfChecked($dayNum) {
+                $isChecked = false;
+                $queryForSchedules = $this->pdo->query("SELECT prefValue FROM settings WHERE prefName = 'schedules'");
+                foreach(explode(', ', $queryForSchedules->fetch(PDO::FETCH_ASSOC)['prefValue']) as $schedule) {
+                    if ($schedule == $dayNum) {
+                        $isChecked = true;
+                    }
+                }
+                return $isChecked;
             }
         }
     }
--- schedules.php
+++ schedules.php
@@ -1,29 +1,23 @@
+<?php
+    include_once 'classes/Scheduler.php';
+    $scheduler = new \pulut\Scheduler();
+    if (isset($_POST['changeScheds'])) {
+        if (isset($_POST['days']) && (is_object($_POST['days']) || is_array($_POST['days']))) {
+            $days = array();
+            foreach ($_POST['days'] as $selected) {
+                array_push($days, $selected);
+            }
+            $scheduler->changeSchedules($days);
+        } else {
+            $scheduler->clearSchedules();
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Welcome to Pulut-Pukyutan!</title>
         <?php include_once 'helpers/gen-inc.php' ?>
-        <script>
-            function submitAddQuery() {
-                var objectName = document.getElementById('objectName-AdderWidget');
-                var objectType = document.getElementById('objectType-AdderWidget');
-                var XMLHttp = new XMLHttpRequest();
-                XMLHttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-
-                    }
-                };
-                XMLHttp.open("POST","async/db_addWasteObj.php", true);
-                XMLHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                XMLHttp.send("objectName="+objectName.value+"&objectType="+objectType.value);
-
-                $('.message .close')
-                    .closest('.message')
-                    .setAttribute('style','position: fixed; top: 10px; left: 30%; right: 30%; z-index: 1000')
-                    .transition('fade')
-                ;
-            }
-        </script>
     </head>
 
 
@@ -31,109 +25,80 @@
         <?php include_once 'components/header.php' ?>
         <div class="fluid container">
            <font style="color: white; font-size: 30px;">Schedules</font>
-                <div class="ui grid">
-                    <div class="four wide column stackable">
+                <div class="ui stackable grid">
+                    <div class="four wide column">
                         <br />
                         <form method="post" action="schedules.php">
                             <div class="ui toggle checkbox" style="border-radius: 10px 10px 0 0; width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="sunday" type="checkbox" name="newsletter">
+                                <input id="sunday" <?php echo $scheduler->getIfChecked($scheduler::MONDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="1">
                                 <label for="sunday" style="color: white !important">Monday</label>
                             </div>
                             <br />
                             <div class="ui toggle checkbox" style="width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="monday" type="checkbox" name="newsletter">
+                                <input id="monday" <?php echo $scheduler->getIfChecked($scheduler::TUESDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="2">
                                 <label for="monday" style="color: white !important">Tuesday</label>
                             </div>
                             <br />
                             <div class="ui toggle checkbox" style="width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="tuesday" type="checkbox" name="newsletter">
-                                <label for="tuesday" style="color: white !important">Tuesday</label>
+                                <input id="tuesday" <?php echo $scheduler->getIfChecked($scheduler::WEDNESDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="3">
+                                <label for="tuesday" style="color: white !important">Wednesday</label>
                             </div>
                             <br />
                             <div class="ui toggle checkbox" style="width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="wednesday" type="checkbox" name="newsletter">
-                                <label for="wednesday"  style="color: white !important">Wednesday</label>
+                                <input id="wednesday" <?php echo $scheduler->getIfChecked($scheduler::THURSDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="4">
+                                <label for="wednesday"  style="color: white !important">Thursday</label>
                             </div>
                             <br />
                             <div class="ui toggle checkbox" style="width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="thursday" type="checkbox" name="newsletter">
-                                <label for="thursday"  style="color: white !important">Thursday</label>
+                                <input id="thursday" <?php echo $scheduler->getIfChecked($scheduler::FRIDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="5">
+                                <label for="thursday"  style="color: white !important">Friday</label>
                             </div>
                             <br />
                             <div class="ui toggle checkbox" style="width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="friday" type="checkbox" name="newsletter">
-                                <label for="friday"  style="color: white !important">Friday</label>
+                                <input id="friday" <?php echo $scheduler->getIfChecked($scheduler::SATURDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="6">
+                                <label for="friday"  style="color: white !important">Saturday</label>
                             </div>
                             <br />
                             <div class="ui toggle checkbox" style="border-radius: 0 0 10px 10px;width: 100%; background-color: rgba(255, 255, 255, 0.2); padding: 10px;">
-                                <input id="saturday" type="checkbox" name="newsletter">
-                                <label for="saturday"  style="color: white !important">Saturday</label>
+                                <input id="saturday" <?php echo $scheduler->getIfChecked($scheduler::SUNDAY) ? 'checked="checked"' : '' ?> type="checkbox" name="days[]" value="7">
+                                <label for="saturday"  style="color: white !important">Sunday</label>
                             </div>
                             <br /><br />
-                            <button class="ui button rounded blue" type="submit">Update Schedules</button>
+                            <button class="ui button rounded blue" name="changeScheds" type="submit">Update Schedules</button>
                         </form>
                         <br /><br />
-                        <font style="font-size: 20px; color: white" id="msg">Schedules of collection updated!</font>
+                        <?php
+                            if (isset($_POST['changeScheds'])) {
+                                echo "<font style=\"font-size: 20px; color: white\" id=\"msg\">Schedules of collection updated!</font>";
+                            }
+                        ?>
+
                     </div>
-                    <div class="twelve wide column stackable">
+                    <div class="twelve wide column">
                         <br />
                         <div style="padding: 10px; height: 100%; width: 100%; border-radius: 10px 10px 0 0; background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(0, 0, 0, 0.1))">
                             <font style="font-size: 20px; color: white">Upcoming Schedules of Trash Collection</font>
                             <br /><br />
-                            <div class="ui grid">
-                                <div class="one wide column" style="color: white">
-                                    <font style="font-size: 23px;">OCT</font>
-                                    <br /><font style="font-size: 40px;">22</font>
-                                </div>
-                                <div class="fifteen wide column" style="color: white">
-                                    <br />
-                                    <font style="font-size: 40px;">Sunday, October 2, 2018 (Asia/Manila, GMT +8:00)</font>
-                                </div>
-                            </div>
-
-                            <div class="ui grid">
-                                <div class="one wide column" style="color: white">
-                                    <font style="font-size: 23px;">OCT</font>
-                                    <br /><font style="font-size: 40px;">22</font>
-                                </div>
-                                <div class="fifteen wide column" style="color: white">
-                                    <br />
-                                    <font style="font-size: 40px;">Sunday, October 2, 2018 (Asia/Manila, GMT +8:00)</font>
-                                </div>
-                            </div>
-
-                            <div class="ui grid">
-                                <div class="one wide column" style="color: white">
-                                    <font style="font-size: 23px;">OCT</font>
-                                    <br /><font style="font-size: 40px;">22</font>
-                                </div>
-                                <div class="fifteen wide column" style="color: white">
-                                    <br />
-                                    <font style="font-size: 40px;">Sunday, October 2, 2018 (Asia/Manila, GMT +8:00)</font>
-                                </div>
-                            </div>
-
-                            <div class="ui grid">
-                                <div class="one wide column" style="color: white">
-                                    <font style="font-size: 23px;">OCT</font>
-                                    <br /><font style="font-size: 40px;">22</font>
-                                </div>
-                                <div class="fifteen wide column" style="color: white">
-                                    <br />
-                                    <font style="font-size: 40px;">Sunday, October 2, 2018 (Asia/Manila, GMT +8:00)</font>
-                                </div>
-                            </div>
-
-                            <div class="ui grid">
-                                <div class="one wide column" style="color: white">
-                                    <font style="font-size: 23px;">OCT</font>
-                                    <br /><font style="font-size: 40px;">22</font>
-                                </div>
-                                <div class="fifteen wide column" style="color: white">
-                                    <br />
-                                    <font style="font-size: 40px;">Sunday, October 2, 2018 (Asia/Manila, GMT +8:00)</font>
-                                </div>
-                            </div>
+                            <?php
+                                foreach ($scheduler->getTwoWeekLongDates(date('Y-m-d')) as $date) {
+                                    if ($scheduler->compareDate($date)) {
+                                        echo "<div class=\"ui stackable grid\">";
+                                            echo "<div class=\"one wide column\" style=\"color: white\">";
+                                            echo "<font style=\"font-size: 23px;\">".$scheduler->returnShortMonth($date)."</font>";
+                                            echo "<br /><font style=\"font-size: 40px;\">".$scheduler->returnDay($date)."</font>";
+                                            echo "</div>";
+                                            echo "<div class=\"computerOnly fifteen wide column\" style=\"overflow-x: hidden; color: white\">";
+                                            echo "<br />";
+                                            echo "<font style=\"white-space: nowrap; font-size: 40px; line-height: 1.2; position: relative; top: -20px;\">".$scheduler->returnLongDateFmt($date)."</font>";
+                                            echo "</div>";
+                                            echo "<div class=\"phoneOnly column\" style=\"overflow-x: hidden; color: white\">";
+                                            echo "<br />";
+                                            echo "<font style=\"font-size: 20px; line-height: 1.2\">".$scheduler->returnLongDateFmt($date)."</font>";
+                                            echo "</div>";
+                                        echo "</div>";
+                                    }
+                                }
+                            ?>
                     </div>
                 </div>
         </div>
